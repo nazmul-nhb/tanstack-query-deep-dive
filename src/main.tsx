a336e01c3c41ad61155c9ab0cc628bcf8fbbf8fb
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-10-01 00:09:37 +0600

    updated staleTime

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,11 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: { staleTime: 1000 * 60 * 3 },
+	},
+});
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
